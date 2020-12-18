0c54bc3d362bb454d9677f8676ba3567c80ed836
johndward01 <johndward01@yahoo.com>
2020-12-18 00:24:42 -0600

    asp.net mvc pt 4 - DONE

--- ASPNET_MVC/Controllers/ProductController.cs
+++ ASPNET_MVC/Controllers/ProductController.cs
@@ -48,6 +48,19 @@ namespace ASPNET_MVC.Controllers
             return RedirectToAction("ViewProduct", new { id = product.ProductID });
         }
 
+        public IActionResult InsertProduct()
+        {
+            var prod = _repo.AssignCategory();
+
+            return View(prod);
+        }
+
+        public IActionResult InsertProductToDatabase(Product productToInsert)
+        {
+            _repo.InsertProduct(productToInsert);
+
+            return RedirectToAction("Index");
+        }
 
 
     }
