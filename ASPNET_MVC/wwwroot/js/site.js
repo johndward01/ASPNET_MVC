b4cfd6e0ef955883dc171929342fa84d27957712
johndward01 <johndward01@gmail.com>
2021-02-11 13:32:43 -0600

    will be replaced by pull

--- ASPNET_MVC/wwwroot/js/site.js
+++ ASPNET_MVC/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let height = window.screen.height;
+let width = window.screen.width;
+
+let h_w = 
