8697cdb7249231bc849b4f0b9c9ac542932107c0
johndward01 <johndward01@yahoo.com>
2021-02-10 23:26:31 -0600

    Task: finish height/width counter

--- ASPNET_MVC/wwwroot/js/site.js
+++ ASPNET_MVC/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+'use strict'
+
+let height = window.screen.height;
+let width = window.screen.width;
+
